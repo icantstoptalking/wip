c496d90d9072feb071d4d41649a130a23e748156
icantstoptalking <icsttemp@gmail.com>
2013-11-01 22:48:52 -0600

    update

--- conf.py
+++ conf.py
@@ -197,7 +197,7 @@ COMPILERS = {
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
 # to the location of conf.py
-# OUTPUT_FOLDER = 'output'
+# OUTPUT_FOLDER = 'c:\users\netmier\documents\github\icantstoptalking.github.io'
 
 # where the "cache" of partial generated content should be located
 # default: 'cache'
